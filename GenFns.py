cd08c0891d2f44a5311e3c98ffb99f742c477fa0
RoverZProduction <portableuro@gmail.com>
2014-11-29 18:47:49 +0100

    Update GenFns.py

--- GenFns.py
+++ GenFns.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 #-*- coding:Utf-8 -*-
 
-#Programme permettant la transformation d'un texte normal en un texte en JSON.
+#Programme qui définit les fonctions utilisés par le script Gen.py
 
-from Genfns import *
-
-withoutColor = text(text = raw_input('Entrez le texte sans couleur :'))
-
-putColor = raw_input('Voulez-vous ajouter de la couleur à un autre texte ? (O/n) :')
-if putColor == 'O':
-    withColor = color(text = raw_input('Entrez le texte à mettre en couleur :'),color = raw_input('Entrez la couleur souhaitée :'))
-    print withoutColor, withColor
-else:
-    print withoutColor + '}'
+def text(text):
+    JSON = ''
+    inter = ['{','text:','"',text,'"']              #Fonction qui transforme une simple phrase en format JSON
+    for fin in inter:
+        JSON = JSON + fin
+    return JSON
+    
+def color(text,color):
+    JSON = ''
+    inter = [',','extra:[{','text:"',text,'",','color:"',color,'"}]}']
+    for fin in inter:                               #Fonction qui permet l'ajout de couleur
+        JSON = JSON + fin
+    return JSON
