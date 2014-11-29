61c2098ae556a1839ba7e693bc11f561a8b4e013
RoverZProduction <portableuro@gmail.com>
2014-11-29 18:47:31 +0100

    Update Gen.py

--- Gen.py
+++ Gen.py
@@ -5,5 +5,11 @@
 
 from Genfns import *
 
-text()
-color()
+withoutColor = text(text = raw_input('Entrez le texte sans couleur :'))
+
+putColor = raw_input('Voulez-vous ajouter de la couleur à un autre texte ? (O/n) :')
+if putColor == 'O':
+    withColor = color(text = raw_input('Entrez le texte à mettre en couleur :'),color = raw_input('Entrez la couleur souhaitée :'))
+    print withoutColor, withColor
+else:
+    print withoutColor + '}'
